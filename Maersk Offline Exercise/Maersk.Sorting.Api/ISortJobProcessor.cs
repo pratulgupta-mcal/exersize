26d6257c9f0ddc8c32fb124841d7b0e15bc5adb9
pratulgupta-mcal <pratul.gupta@mcal.in>
2020-10-08 20:38:11 +0530

    New Solution Added

---
+++ Maersk Offline Exercise/Maersk.Sorting.Api/ISortJobProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Maersk.Sorting.Api
+{
+    public interface ISortJobProcessor
+    {
+        Task<SortJob> Process(SortJob job);
+
+        void Enqueue(SortJob job);
+
+        Task ProcessQueue();
+
+        List<SortJob> GetAllJobs();
+
+        SortJob GetJobById(Guid id);
+    }
+}
